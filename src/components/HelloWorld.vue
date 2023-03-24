d926f6c799798653e36074fd6cd1fed1cff03b09
Alperen kır <alperenkr28@gmail.com>
2023-03-24 18:46:53 +0300

    add gif and update design

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="hello">
-<!--    <img :src="'/assets/img/2.png'" alt="">-->
-    <img alt="Vue logo" :src="getImage(firstDice)">
-    <img alt="Vue logo" :src="getImage(secondDice)">
-    <button @click="rollDice">SALLA</button>
+  <div class="hello" v-if="!gifIsActive">
+    <img alt="zar" :src="getImage(firstDice)">
+    <img alt="zar" :src="getImage(secondDice)">
   </div>
+  <div class="hello" v-if="gifIsActive">
+    <img alt="zar" :src="getImage(0)">
+    <img alt="zar" :src="getImage(0)">
+  </div>
+  <div id="total">
+<span>Toplam: {{ total }}</span>
+  </div>
+  <button id="dice-button" @click="rollDice">Zar At</button>
 </template>
 
 <script>
@@ -18,17 +24,39 @@ export default {
     return {
       firstDice: Math.floor(Math.random() * 6) + 1,
       secondDice: Math.floor(Math.random() * 6) + 1,
+      showGif: false,
+      total: this.firstDice + this.secondDice
     };
   },
+  computed: {
+    gifIsActive() {
+
+      return this.showGif;
+    },
+  },
+  mounted() {
+    this.total =this.firstDice + this.secondDice;
+  },
+
   methods: {
     getImage(diceNumber) {
+      if (diceNumber === 0) {
+        return require("../assets/zar.gif");
 
+      }
       return require("../assets/" + diceNumber + ".png");
     },
-    rollDice() {
+    async rollDice() {
+      this.showGif = true;
       this.firstDice = Math.floor(Math.random() * 6) + 1;
       this.secondDice = Math.floor(Math.random() * 6) + 1;
-    }
+
+      await new Promise(resolve => setTimeout(resolve, 1200));
+
+      this.total =this.firstDice + this.secondDice;
+
+      this.showGif = false;
+    },
 
   }
 }
@@ -36,17 +64,35 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+img {
+  width: 50%;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
+}
+ #dice-button {
+   font-size: 24px;
+   padding: 12px 24px;
+   margin-top: 15px;
+   background-color: #008CBA;
+   color: white;
+   border: none;
+   border-radius: 15%;
+   cursor: pointer;
+ }
+
+#total {
+  font-size: 24px;
+  margin-top: 15px;
+  font-weight: bold;
 }
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
